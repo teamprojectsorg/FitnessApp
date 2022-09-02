749321866fceb82b689a7fa61ce6c67e4c857995
Kanav Gupta <64695069+KanavGupta01@users.noreply.github.com>
2022-09-02 11:09:03 +0530

    Revert "initial commit"
    This reverts commit 5e89a1dac760889057e79ec67caa440cb0153ead.

--- Api/utils/database.js
+++
@@ -1,3 +0,0 @@
-const Sequelize = require('sequelize')
-
-module.exports = new Sequelize('postgres://postgres:postgres@localhost:5432/FitnessApp')
--- Api/utils/queryResult.js
+++
@@ -1,9 +0,0 @@
-function queryResult(status, message, data = null) {
-    return {
-        status: status,
-        message: message,
-        data: data
-    }
-}
-
-module.exports = queryResult
