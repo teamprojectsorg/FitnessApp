8c73518aa95560504f01d40cece3a84780715b8d
kashyapjain <kashyapjain2018@gmail.com>
2022-09-03 13:36:14 +0530

    Merged Changes

--- Android/app/src/main/java/com/fitnessapp/pages/home/HomeFragment.java
+++ Android/app/src/main/java/com/fitnessapp/pages/home/HomeFragment.java
@@ -8,6 +8,8 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
+import com.fitnessapp.databinding.FragmentHomeBinding;
+
 public class HomeFragment extends Fragment {
     FragmentHomeBinding viewBinding;
     @Override
