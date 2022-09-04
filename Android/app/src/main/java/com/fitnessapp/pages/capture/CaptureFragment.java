0046df9a1cc984c6384281bc4c490777e149e86b
Amanpreet Kaur <amanpreet2516@gmail.com>
2022-09-04 12:23:31 +0530

    update

--- Android/app/src/main/java/com/fitnessapp/pages/capture/CaptureFragment.java
+++ Android/app/src/main/java/com/fitnessapp/pages/capture/CaptureFragment.java
@@ -7,14 +7,63 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
 
 import com.fitnessapp.R;
+import com.fitnessapp.databinding.FragmentCaptureBinding;
+import com.fitnessapp.databinding.FragmentSignupBinding;
 
 public class CaptureFragment extends Fragment {
+    Spinner spinner;
+    Spinner spinner1;
+    String[] intakemood = {"Happy", "Sad", "Angry", "Occasionally", "Nothing/Fun" };
+    String[] intaketype = {"Alcohol", "Beer", "Whiskey"};
+    FragmentCaptureBinding viewBinding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_capture, container, false);
+        viewBinding = FragmentCaptureBinding.inflate(inflater,container,false);
+
+        spinner = viewBinding.spinner;
+
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_spinner_item,intaketype);
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner.setAdapter(adapter);
+
+        spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String value = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+        //for intake mood
+        spinner1 =
+        spinner1 = viewBinding.spinnerwhy;
+
+        ArrayAdapter<String> adapter1 = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_spinner_item,intakemood);
+        adapter1.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spinner1.setAdapter(adapter1);
+
+        spinner1.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+            @Override
+            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                String value1 = parent.getItemAtPosition(position).toString();
+            }
+
+            @Override
+            public void onNothingSelected(AdapterView<?> parent) {
+
+            }
+        });
+
+        return viewBinding.getRoot();
     }
 }
--- Android/app/src/main/java/com/fitnessapp/pages/profile/ProfileFragment.java
+++ Android/app/src/main/java/com/fitnessapp/pages/profile/ProfileFragment.java
@@ -1,16 +1,25 @@
 package com.fitnessapp.pages.profile;
 
+import android.content.Intent;
 import android.os.Bundle;
 
+import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
+import androidx.navigation.Navigation;
 
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.fitnessapp.R;
-public class ProfileFragment extends Fragment {
+import com.fitnessapp.databinding.FragmentProfileBinding;
+import com.fitnessapp.pages.capture.CaptureFragment;
+import com.fitnessapp.pages.edit_profile.EditProfileFragment;
 
+public class ProfileFragment extends Fragment implements View.OnClickListener{
+
+    private CardView editProfileCard, capturizerCard;
+    FragmentProfileBinding viewBinding;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -20,6 +29,34 @@ public class ProfileFragment extends Fragment {
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_profile, container, false);
+        viewBinding = FragmentProfileBinding.inflate(inflater,container,false);
+        initCard();
+        return viewBinding.getRoot();
+    }
+    private void initCard()
+    {
+        editProfileCard =viewBinding.editProfileCard;
+        capturizerCard = viewBinding.capturizerCard;
+        //add click listener to the cards
+        editProfileCard.setOnClickListener((v)->editProfileClick());
+        capturizerCard.setOnClickListener((v)->catureClick());
+    }
+    public void editProfileClick()
+    {
+        Navigation.findNavController(viewBinding.getRoot()).navigate(R.id.action_profileFragment_to_editProfileFragment);
+    }
+    public void catureClick()
+    {
+        Navigation.findNavController(viewBinding.getRoot()).navigate(R.id.action_profileFragment_to_captureFragment);
+    }
+    @Override
+    public void onClick(View v) {
+        Intent i;
+
+        switch (v.getId()) {
+            case R.id.edit_profile_card: Navigation.findNavController(v).navigate(R.id.action_profileFragment_to_editProfileFragment);
+            case R.id.capturizer_card: Navigation.findNavController(v).navigate(R.id.action_profileFragment_to_captureFragment);
+            default:break;
+        }
     }
 }
