53d2b4fa9723557d261426fc2b2cf92a92df702c
kashyapjain <58132729+kashyapjain@users.noreply.github.com>
2022-09-09 13:11:44 +0530

    Revert "Iteration 1 complete updates (#46)"
    This reverts commit 2d4c8c2d24d28a5b80f6eb81de08e4046e98a359.

--- Android/app/src/main/java/com/fitnessapp/pages/login_signup/LoginSignUpAPI.java
+++ Android/app/src/main/java/com/fitnessapp/pages/login_signup/LoginSignUpAPI.java
@@ -5,16 +5,13 @@ import com.fitnessapp.pages.login_signup.models.LoginSignUpModel;
 import com.fitnessapp.pages.login_signup.models.SignUpResponseModel;
 
 import retrofit2.Call;
-
 import retrofit2.http.Body;
 import retrofit2.http.POST;
 
 public interface LoginSignUpAPI {
-
     @POST("auth/signup")
     Call<SignUpResponseModel> signUp(@Body LoginSignUpModel signUpModel);
 
     @POST("auth/login")
     Call<LoginResponseModel> logIn(@Body LoginSignUpModel logInModel);
-
 }
--- Android/app/src/main/java/com/fitnessapp/pages/profile/ProfileFragment.java
+++ Android/app/src/main/java/com/fitnessapp/pages/profile/ProfileFragment.java
@@ -7,13 +7,11 @@ import androidx.cardview.widget.CardView;
 import androidx.fragment.app.Fragment;
 import androidx.navigation.Navigation;
 
-
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.fitnessapp.R;
-
 import com.fitnessapp.databinding.FragmentProfileBinding;
 import com.fitnessapp.pages.capture.CaptureFragment;
 import com.fitnessapp.pages.edit_profile.EditProfileFragment;
@@ -22,7 +20,6 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
 
     private CardView editProfileCard, capturizerCard;
     FragmentProfileBinding viewBinding;
-
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -32,7 +29,6 @@ public class ProfileFragment extends Fragment implements View.OnClickListener{
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
         // Inflate the layout for this fragment
-
         viewBinding = FragmentProfileBinding.inflate(inflater,container,false);
         initCard();
         return viewBinding.getRoot();
