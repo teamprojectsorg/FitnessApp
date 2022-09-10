44dc97de7e5df27138b044acb28a11d2d1bde54b
Amanpreet Kaur <amanpreet2516@gmail.com>
2022-09-10 10:05:34 +0530

    updates in capture

--- Android/app/src/main/java/com/fitnessapp/pages/capture/CaptureFragment.java
+++ Android/app/src/main/java/com/fitnessapp/pages/capture/CaptureFragment.java
@@ -8,6 +8,7 @@ import androidx.navigation.Navigation;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Adapter;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Spinner;
@@ -18,12 +19,14 @@ import com.fitnessapp.databinding.FragmentCaptureBinding;
 import com.fitnessapp.databinding.FragmentSignupBinding;
 
 public class CaptureFragment extends Fragment {
-    Spinner spinner;
-    Spinner spinner1;
-    Spinner spinnerAlcohol;
+    Spinner spinner , spinner1, spinnerAlcohol, quantitySpinner;
     String[] alcohol_percentage = {"3 - 6%","10 - 15%","35 - 50%"};
     String[] intakemood = {"Happy", "Sad", "Angry", "Occasionally", "Nothing/Fun" };
-    String[] intaketype = { "Beer: more than 7%", "Whiskey: 40% - 50%", "Wine: 5% - 25%"};
+    String[] intaketype = { "Beer", "Whiskey", "Wine"};
+    String[] beerQuantity = {"285ml", "375ml", "425ml"};
+    String[] whiskyQuantity = {"30ml", "275ml", "700ml"};
+    String[] wineQuantity = {"100ml", "150ml", "750ml"};
+
     FragmentCaptureBinding viewBinding;
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
@@ -32,15 +35,70 @@ public class CaptureFragment extends Fragment {
         viewBinding.btnSubmit.setOnClickListener((v)-> Navigation.findNavController(v).popBackStack());
 
         spinner = viewBinding.spinner;
-
+        quantitySpinner = viewBinding.spinnerQuantity;
+        //type of alcohol spinner
         ArrayAdapter<String> adapter = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_spinner_item,intaketype);
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
         spinner.setAdapter(adapter);
+        //dependent spinner on type - beer quantity spinner
+        ArrayAdapter<String> adapterQuantityBeer = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_spinner_item , beerQuantity);
+        adapterQuantityBeer.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //dependent spinner on type - whisky spinner
+        ArrayAdapter<String> adapterQuantityWhisky = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_spinner_item,whiskyQuantity);
+        adapterQuantityWhisky.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        //dependent spinner on type - wine spinner
+        ArrayAdapter<String> adapterQuantityWine = new ArrayAdapter<String>(this.getContext(), android.R.layout.simple_spinner_item,wineQuantity);
+        adapterQuantityWine.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
 
+        //Item selected listener on type of alcohol
         spinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
             @Override
             public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
                 String value = parent.getItemAtPosition(position).toString();
+                if(position==0) {
+                    quantitySpinner.setAdapter(adapterQuantityBeer);
+                    quantitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            String value1 = parent.getItemAtPosition(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                }
+                else if(position==1) {
+                    quantitySpinner.setAdapter(adapterQuantityWhisky);
+                    quantitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            String value2 = parent.getItemAtPosition(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                }
+                else{
+                    quantitySpinner.setAdapter(adapterQuantityWine);
+                    quantitySpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener() {
+                        @Override
+                        public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
+                            String value3 = parent.getItemAtPosition(position).toString();
+                        }
+
+                        @Override
+                        public void onNothingSelected(AdapterView<?> parent) {
+
+                        }
+                    });
+                }
+
+
             }
 
             @Override
--- Android/app/src/main/java/com/fitnessapp/pages/help_center/HelpCenterFragment.java
+++ Android/app/src/main/java/com/fitnessapp/pages/help_center/HelpCenterFragment.java
@@ -2,6 +2,7 @@ package com.fitnessapp.pages.help_center;
 
 import android.os.Bundle;
 
+import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
 import android.view.LayoutInflater;
@@ -9,58 +10,24 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import com.fitnessapp.R;
+import com.fitnessapp.databinding.FragmentHelpCenterBinding;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.OnMapReadyCallback;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.LatLng;
 
-/**
- * A simple {@link Fragment} subclass.
- * Use the {@link HelpCenterFragment#newInstance} factory method to
- * create an instance of this fragment.
- */
 public class HelpCenterFragment extends Fragment {
 
-    // TODO: Rename parameter arguments, choose names that match
-    // the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-    private static final String ARG_PARAM1 = "param1";
-    private static final String ARG_PARAM2 = "param2";
-
-    // TODO: Rename and change types of parameters
-    private String mParam1;
-    private String mParam2;
-
-    public HelpCenterFragment() {
-        // Required empty public constructor
-    }
-
-    /**
-     * Use this factory method to create a new instance of
-     * this fragment using the provided parameters.
-     *
-     * @param param1 Parameter 1.
-     * @param param2 Parameter 2.
-     * @return A new instance of fragment HelpCenterFragment.
-     */
-    // TODO: Rename and change types and number of parameters
-    public static HelpCenterFragment newInstance(String param1, String param2) {
-        HelpCenterFragment fragment = new HelpCenterFragment();
-        Bundle args = new Bundle();
-        args.putString(ARG_PARAM1, param1);
-        args.putString(ARG_PARAM2, param2);
-        fragment.setArguments(args);
-        return fragment;
-    }
-
+    //GoogleMap map;
+    FragmentHelpCenterBinding viewBinding;
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getArguments() != null) {
-            mParam1 = getArguments().getString(ARG_PARAM1);
-            mParam2 = getArguments().getString(ARG_PARAM2);
-        }
     }
-
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_help_center, container, false);
+        viewBinding = FragmentHelpCenterBinding.inflate(inflater, container, false);
+        return viewBinding.getRoot();
     }
 }
