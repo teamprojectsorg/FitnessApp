ce92e6b60f0d055b820411cec34ff481d11e3a6f
kashyapjain <kashyapjain2018@gmail.com>
2022-09-11 12:00:28 +0530

    line graph backend and target line

--- Android/app/src/main/java/com/fitnessapp/pages/capture/models/CaptureModel.java
+++ Android/app/src/main/java/com/fitnessapp/pages/capture/models/CaptureModel.java
@@ -3,7 +3,7 @@ package com.fitnessapp.pages.capture.models;
 public class CaptureModel {
     public String drinkIntake;
     public String drinkIntension;
+    public String alcoholPercentage;
     public String date;
     public String drinkName;
-    public String diseaseRisk;
 }
